fabd9b36c89485b27297e650e44db663851c97f3
Quangne <22162025@student.hcmute.edu.vn>
2024-05-21 13:21:53 +0700

    Quang

--- app/src/main/java/com/example/realestateapp/API/ApiService.java
+++ app/src/main/java/com/example/realestateapp/API/ApiService.java
@@ -25,7 +25,7 @@ public interface ApiService {
             .create();
 
     ApiService apiService = new Retrofit.Builder()
-            .baseUrl("http://192.168.1.171:8810/api/")
+            .baseUrl("http://172.16.30.73:8810/api/")
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
             .create(ApiService.class);
